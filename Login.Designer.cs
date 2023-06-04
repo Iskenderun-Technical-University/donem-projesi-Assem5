eda7c12a528822897302583da2fafd39c26ada99
Assem5 <47611958+Assem5@users.noreply.github.com>
2023-06-04 19:38:19 +0300

    Add files via upload

---
+++ Login.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Book_Control
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            UNameTb = new TextBox();
+            UPassTb = new TextBox();
+            bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(64, 64, 64);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(148, 324);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(0, -1);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(147, 322);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 13;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Liberation Sans", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(64, 64, 64);
+            label1.Location = new Point(188, 5);
+            label1.Name = "label1";
+            label1.Size = new Size(171, 23);
+            label1.TabIndex = 1;
+            label1.Text = "IT BOOK STORE";
+            label1.Click += label1_Click;
+            // 
+            // UNameTb
+            // 
+            UNameTb.Location = new Point(261, 139);
+            UNameTb.Name = "UNameTb";
+            UNameTb.Size = new Size(139, 23);
+            UNameTb.TabIndex = 3;
+            // 
+            // UPassTb
+            // 
+            UPassTb.Location = new Point(261, 182);
+            UPassTb.Name = "UPassTb";
+            UPassTb.PasswordChar = '*';
+            UPassTb.Size = new Size(139, 23);
+            UPassTb.TabIndex = 4;
+            // 
+            // bunifuThinButton21
+            // 
+            bunifuThinButton21.ActiveBorderThickness = 1;
+            bunifuThinButton21.ActiveCornerRadius = 1;
+            bunifuThinButton21.ActiveFillColor = Color.FromArgb(64, 64, 64);
+            bunifuThinButton21.ActiveForecolor = Color.White;
+            bunifuThinButton21.ActiveLineColor = Color.FromArgb(64, 64, 64);
+            bunifuThinButton21.BackColor = SystemColors.Control;
+            bunifuThinButton21.BackgroundImage = (Image)resources.GetObject("bunifuThinButton21.BackgroundImage");
+            bunifuThinButton21.ButtonText = "Login";
+            bunifuThinButton21.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            bunifuThinButton21.ForeColor = Color.SeaGreen;
+            bunifuThinButton21.IdleBorderThickness = 1;
+            bunifuThinButton21.IdleCornerRadius = 20;
+            bunifuThinButton21.IdleFillColor = Color.White;
+            bunifuThinButton21.IdleForecolor = Color.FromArgb(64, 64, 64);
+            bunifuThinButton21.IdleLineColor = Color.FromArgb(64, 64, 64);
+            bunifuThinButton21.Location = new Point(267, 213);
+            bunifuThinButton21.Margin = new Padding(5);
+            bunifuThinButton21.Name = "bunifuThinButton21";
+            bunifuThinButton21.Size = new Size(133, 35);
+            bunifuThinButton21.TabIndex = 5;
+            bunifuThinButton21.TextAlign = ContentAlignment.MiddleCenter;
+            bunifuThinButton21.Click += bunifuThinButton21_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(64, 64, 64);
+            label2.Location = new Point(154, 139);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 21);
+            label2.TabIndex = 6;
+            label2.Text = "UserName";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(64, 64, 64);
+            label3.Location = new Point(154, 182);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 21);
+            label3.TabIndex = 7;
+            label3.Text = "Password";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(64, 64, 64);
+            label4.Location = new Point(402, -5);
+            label4.Name = "label4";
+            label4.Size = new Size(25, 30);
+            label4.TabIndex = 8;
+            label4.Text = "X";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Underline, GraphicsUnit.Point);
+            label5.ForeColor = Color.FromArgb(64, 64, 64);
+            label5.Location = new Point(303, 245);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 25);
+            label5.TabIndex = 9;
+            label5.Text = "Admin";
+            label5.Click += label5_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(216, 30);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(123, 43);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 12;
+            pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(422, 322);
+            Controls.Add(pictureBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(bunifuThinButton21);
+            Controls.Add(UPassTb);
+            Controls.Add(UNameTb);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            HelpButton = true;
+            KeyPreview = true;
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += Login_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private TextBox UNameTb;
+        private TextBox UPassTb;
+        private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book_Control
+{
+    public partial class Users : Form
+    {
+        public Users()
+        {
+            InitializeComponent();
+            populate();
+            UserDGV.CellContentClick += new DataGridViewCellEventHandler(UserDGV_CellContentClick);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\ahabd\source\repos\Book Control\Book Control\Database1.mdf;Integrated Security = True");
+
+        private void populate()
+        {
+            con.Open();
+            string query = "select * from UserTbl";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            UserDGV.DataSource = ds.Tables[0];
+            con.Close();
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            if (UnameTb.Text == "" || AddTb.Text == "" || PassTb.Text == "" || PhoneTb.Text == "")
+            {
+                MessageBox.Show("Missing Information");
+
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    string query = "insert into UserTbl values('" + UnameTb.Text + "','" + PhoneTb.Text + "','" + AddTb.Text + "','" + PassTb.Text + "')";
+                    // "insert into BookTbl values('" + BtitleTb.Text + "','" + BautTb.Text + "','" + BCatCb.SelectedItem.ToString() + "'," + QtyTb.Text + "," + PriceTb.Text + ")";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("User Saved Successfully ");
+                    con.Close();
+                    populate();
+                }
+                catch (Exception Ex)
+
+                {
+                    MessageBox.Show(Ex.Message);
+
+                }
+
+            }
+        }
+
+        private void Users_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Reset()
+        {
+            UnameTb.Text = "";
+            PassTb.Text = "";
+            PhoneTb.Text = "";
+            AddTb.Text = "";
+
+        }
+
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            Reset();
+
+        }
+
+
+        int key = 0;
+        private void UserDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = UserDGV.Rows[e.RowIndex];
+                UnameTb.Text = row.Cells[1].Value.ToString();
+                PhoneTb.Text = row.Cells[2].Value.ToString();
+                AddTb.Text = row.Cells[3].Value.ToString();
+                PassTb.Text = row.Cells[4].Value.ToString();
+
+                if (int.TryParse(row.Cells[0].Value.ToString(), out int selectedKey))
+                {
+                    key = selectedKey;
+                }
+                else
+                {
+                    key = 0;
+                }
+            }
+        }
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            if (key == 0)
+            {
+                MessageBox.Show("No row or cell selected.");
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    string query = "DELETE FROM UserTbl WHERE UId = @Key";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Key", key);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Book Deleted Successfully");
+                    con.Close();
+                    populate();
+                    Reset();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void EditBtn_Click(object sender, EventArgs e)
+        {
+            if (UnameTb.Text == "" || AddTb.Text == "" || PassTb.Text == "" || PhoneTb.Text == "")
+            {
+                MessageBox.Show("No row or cell selected.");
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    string query = "update UserTbl set UName = '" + UnameTb.Text + "',UPhone='" + PhoneTb.Text + "',UAdd='" + AddTb.Text + "',UPass='" + PassTb.Text + "' where UId=" + key + ";";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Key", key);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("UserTbl Updated Successfully");
+                    con.Close();
+                    populate();
+                    Reset();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            Login obj = new Login();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Books obj = new Books();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Users obj = new Users();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            DashBoard obj = new DashBoard();
+            obj.Show();
+            this.Hide();
+        }
+    }
+}
